8a09d982d0ce38dff3b1c37e533191e7a110a339
ayoshii27 <dayoshii27@gmail.com>
2024-02-04 16:39:49 +0900

    delete: 不要な依存ライブラリを削除する（graphql, prisma）

--- app/expends/layout.tsx
+++ app/expends/layout.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import { ApolloProvider } from "@apollo/client";
-import apolloClient from "../../lib/apollo";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 export default function Home({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ApolloProvider client={apolloClient}>{children}</ApolloProvider>
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
