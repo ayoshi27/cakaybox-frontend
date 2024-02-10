d377091990e5231db44b3d71fb865ae7d03fee77
ayoshii27 <dayoshii27@gmail.com>
2024-02-10 19:11:38 +0900

    fix: apiリクエストのurlを変更する

--- lib/query.ts
+++ lib/query.ts
@@ -2,6 +2,6 @@ import { initQueryClient } from "@ts-rest/react-query";
 import { contract } from "./contract/contract";
 
 export const client = initQueryClient(contract, {
-  baseUrl: "http://localhost:3000",
+  baseUrl: "/api",
   baseHeaders: {},
 });
